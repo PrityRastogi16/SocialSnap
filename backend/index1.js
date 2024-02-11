9cd8ddea3ae87b4f2e218b4b8d3d6ac79d916035
shraddha-gawde <shraddha.gawde1999@gmail.com>
2024-02-11 15:16:27 +0530

    facebook login

--- backend/index1.js
+++ backend/index1.js
@@ -1,28 +1,3 @@
-// const express = require('express');
-// const {connection}=require('./db');
-
-// const cors = require("cors")
-
-// const app = express()
-// app.use(cors())
-// app.use(express.json());
-
-
-// app.get('/',(req,res)=>{
-//     res.send("This is Home")
-// })
-     
-// app.listen(6420,async()=>{
-//     try{
-//          await connection
-//          console.log("connected to db")
-//          console.log("server is running at port 8080");
-//     }catch(err){
-//         console.log(err);
-//     }
-    
-// })
-
 const express = require('express');
 const app = express();
 const session = require('express-session');
