54b38310c2cb7c8ff0dceaab798d3205c9b948e8
aditi-keerti <aditibhadoriya1333@gmail.com>
2024-02-12 13:21:47 +0530

    day1

--- backend/index.js
+++ backend/index.js
@@ -1,55 +1,35 @@
-// const express = require('express');
-// const {connection}=require('./db');
-
-// const cors = require("cors")
-
-// const app = express()
-// app.use(cors())
-// app.use(express.json());
-
-
-// app.get('/',(req,res)=>{
-//     res.send("This is Home")
-// })
-     
-// app.listen(6420,async()=>{
-//     try{
-//          await connection
-//          console.log("connected to db")
-//          console.log("server is running at port 8080");
-//     }catch(err){
-//         console.log(err);
-//     }
-    
-// })
-
 const express = require('express');
+const http=require('http');
+const socketIo=require('socket.io');
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+require('dotenv').config();
 const session = require('express-session');
+const passport = require('passport');
+const cors=require('cors');
+const {join}=require('path');
+
+// auth code
+
 const {connection}=require('./db');
-
 const facebookRouter = require('./controllers/routes/facebook.route');
-
 const {postRouter} = require("./controllers/routes/post.routes")
 const {likeRouter} = require("./controllers/routes/like.routes")
+const PORT=process.env.PORT;
 
 
-const passport = require('passport');
 
 
-const app = express()
+
 app.use(cors())
 app.use(express.json());
+
 app.use('/posts', postRouter)
 app.use('/like',likeRouter)
 
 
-require('dotenv').config();
-
 app.set('view engine', 'ejs');
-
-
-
 app.use(
   session({
     resave: false,
@@ -57,10 +37,8 @@ app.use(
     secret: process.env.SESSION_SECRET,
   })
 );
-
 app.use(passport.initialize());
 app.use(passport.session());
-
 passport.serializeUser(function (user, cb) {
   cb(null, user);
 });
@@ -72,12 +50,31 @@ app.get('/', (req, res) => {
   res.render('auth');
 });
 
-// app.use('/auth/google', authRouter);
 app.use('/auth/facebook', facebookRouter);
-// app.use('/auth/github', githubRouter);
-// app.use('/protected', protectedRouter);
 
-app.listen(6420,async()=>{
+
+// aditi's code
+app.get('/chat',(req,res)=>{
+  res.sendFile(join(__dirname,'../frontend/views/client.html'))
+})
+
+io.on('connection',(socket)=>{
+  console.log('a user is connected')
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+})
+
+
+
+
+
+
+
+
+
+
+server.listen(PORT,async()=>{
     try{
          await connection
          console.log("connected to db")
