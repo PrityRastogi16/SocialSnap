41d50cdab33930a9a6d6a0337fc275b7798e0e7b
aditi-keerti <aditibhadoriya1333@gmail.com>
2024-02-12 23:52:24 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ const {likeRouter} = require("./controllers/routes/like.routes")
 const passport = require('passport');
 
 
-const app = express()
+
 app.use(cors())
 app.use(express.json());
 app.use('/posts', postRouter)
