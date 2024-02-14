f3af2ef46af3df85a701f5acfb32808ec6cdd33f
PrityRastogi16 <surajyoti9839@gmail.com>
2024-02-15 01:41:40 +0530

    aggregations

--- backend/controllers/routes/post.routes.js
+++ backend/controllers/routes/post.routes.js
@@ -3,7 +3,8 @@ const {uploadMiddleware} = require("../middlewares/fileUpload");
 const {MediaModel, PostModel} = require("../../models/post.models")
 const base_url = "http://localhost:6420/"
 const postRouter = express.Router();
-const {auth} = require("../middlewares/auth")
+const {auth} = require("../middlewares/auth");
+const { default: mongoose } = require("mongoose");
 
 // CREATE POST
 postRouter.post("/create_post/:userID", auth,uploadMiddleware, async(req,res)=>{
@@ -32,8 +33,55 @@ postRouter.post("/create_post/:userID", auth,uploadMiddleware, async(req,res)=>{
 
 // GET ALL POSTS
 postRouter.get("/all_post", async(req,res)=>{
+  const limit = parseInt(req.query.limit) || 10;
+  const page = parseInt(req.query.page) || 1;
+  const user_id = req.query.user_id;
+
+  const totalPosts = await PostModel.countDocuments({})
+  const totalPages = Math.ceil(totalPosts/limit)
+  const startIdx = (page-1) * limit
+
   try{
-    const posts = await PostModel.find({});
+    const posts = await PostModel.aggregate([
+    {
+       $lookup: {
+          from:"likes", let :{post_id:"$_id"}, pipeline: [
+             {
+              $match:{
+                 $expr:{
+                    $and:[
+                      {$eq:['$post_id','$post_id']},
+                      {$eq:['$user_id',new mongoose.Types.ObjectId(user_id)]}
+                    ]
+                 },
+              },
+             }
+          ],
+          as:'likes'
+       },
+    },
+    {
+      $addFields:{
+        isLike:{
+          $cond:{
+            if:{$gt:[{size:'$likes'},0]},
+            then: true,
+            else: false
+          }
+        }
+      }
+    },
+    {
+       $project:{likes:0}
+    },
+    {
+      $skip:(page-1)*limit,
+    },{
+     $limit:limit,
+    },{
+      $sort:{createdAt:-1}
+    },
+    ])
     res.status(200).json({msg:"All Post", data:posts})
   }
   catch(err){
